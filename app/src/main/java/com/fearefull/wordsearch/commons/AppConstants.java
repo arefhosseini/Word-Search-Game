5f88e9595bb1d4ae5e2eb2d2b31080cb567719a2
Aref <arefhosseini@yahoo.com>
2020-05-13 16:18:11 +0430

    change Base Url

--- app/src/main/java/com/fearefull/wordsearch/commons/AppConstants.java
+++ app/src/main/java/com/fearefull/wordsearch/commons/AppConstants.java
@@ -1,7 +1,8 @@
 package com.fearefull.wordsearch.commons;
 
 public final class AppConstants {
-    public static final String BASE_URL = "http://10.0.2.2:8000/";
+//    public static final String BASE_URL = "http://10.0.2.2:8000/";
+    public static final String BASE_URL = "http://farsbase.net/gr/";
 
     public static final int CONNECT_CLIENT = 120;
     public static final int MAXIMUM_COL_LENGTH = 12;
